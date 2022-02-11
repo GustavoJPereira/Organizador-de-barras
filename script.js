d2ba3fd128f5c174235c2780f1daeb70e86d102a
GustavoJPereira <gustavojosepereira2021@gmail.com>
2022-02-10 21:28:03 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const TELA = document.querySelector('.tela');
+var lista = new Array(3, 1, 2, 12, 10, 15, 14, 2);
+
+var ladoBarras = (TELA.clientWidth + 20) / lista.length - 20;
+
+// Descobre a barra que terá o maior valor
+var alturaMaximoBarras;
+
+function descobrirMaior() {
+
+    let maiorNumeroDaLista = null;
+
+    for (let contadora = 1; contadora <= lista.length; contadora++) {
+        if (lista[contadora - 1] > maiorNumeroDaLista) {
+            maiorNumeroDaLista = lista[contadora - 1];
+        }
+    }
+
+    alturaMaximoBarras = maiorNumeroDaLista;
+}
+
+descobrirMaior();
+
+// cria as barras no HTML
+function criarBarras() {
+    for (let contadora = 1; contadora <= lista.length; contadora++) {
+        novaBarra = document.createElement('div')
+        
+        TELA.appendChild(novaBarra)
+
+        novaBarra.style.height = (lista[contadora - 1] / (alturaMaximoBarras / 100)) + "%";
+        novaBarra.style.width = ladoBarras + "px";
+
+    }
+}
+
+criarBarras();
